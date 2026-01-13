34ae273b7875725c95460a0a9fa49dc65046f44e
mohd750arkham <arkham750@gmail.com>
2026-01-13 17:32:20 +0530

    adding on 13th jan

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base : "https://github.com/mohd750arkham/portfolio.git/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
